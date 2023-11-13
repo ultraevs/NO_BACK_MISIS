e590d865d290deb42e8f82049d683458e68ec80d
огузок <128810079+themeppci@users.noreply.github.com>
2023-11-13 14:23:37 +0300

    +img backup

--- parking/get_image.py
+++ parking/get_image.py
@@ -33,7 +33,7 @@ def download(place_id):
     # downloading video from camera
     links = {
         0: 'https://s2.moidom-stream.ru/s/public/0000007683-',
-        1: 'http://136.169.144.5/1531895611/tracks-v1/index.fmp4.m3u8?token=9a6e23ca4b574e57ba0d9ebd04aff670',
+        1: 'http://136.169.144.33/1549021886/tracks-v1/index.fmp4.m3u8?token=9d3a33efff5341b18f1c8bae5f478779',
         2: 'http://136.169.144.3/1549021886/tracks-v1/index.fmp4.m3u8?token=e3bc0fbf2d674cfc85a09be042ebb6c7'
     }
     status = False
@@ -43,40 +43,13 @@ def download(place_id):
             capture = cv2.VideoCapture(links[place_id])
             ret, frame = capture.read()
             status = True
+            cv2.imwrite(f"img_{place_id}.jpg", frame)
             print('[download] link success, downloaded video')
-            if os.path.exists('img.jpg'):
-                os.remove('img.jpg')
-            cv2.imwrite("img.jpg", frame)
         except Exception as e:
             print('[download] link failed')
             print(e)
             status = False
-    else:
-        print('PLACE_ID=0 IS OUTDATED, USE PLACE_ID=1/2')
-        for second in range(0, 60):
-            if len(str(second)) == 1:
-                second = '0' + str(second)
-            try:
-                if place_id == 0:
-                    time = f'{generate_time(place_id)}{second}.ts'
-                elif place_id == 1:
-                    time = f'{generate_time(place_id)}{second}-12308.ts'
-                
-                link = links[place_id] + time
-                print(f'trying: {link}')
-                urllib.request.urlretrieve(
-                    link,
-                    'camera_feed.mp4'
-                )
-                # if uccessfully downloaded, delete previous image
-                print('[download] link success, downloaded video')
-                if os.path.exists('img.jpg'):
-                    os.remove('img.jpg')
-                status = True
-                break
 
-            except Exception as e:
-                print('[download] link failed')
     return status
 
 def get_image(place_id: int):
