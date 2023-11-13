faed2961ca6ee3c9ba6bd6306acd21d302135711
огузок <128810079+themeppci@users.noreply.github.com>
2023-11-13 21:06:52 +0300

    example

--- parking/core.py
+++ parking/core.py
@@ -1,8 +1,26 @@
 from detection import parking_info
 from ultralytics import YOLO
 
-model = YOLO('modelv2.pt')
 
+# импортировать конкретно эту модель
+model = YOLO('segmentation.pt')
 
 answer = parking_info(model, 1)
+
 print(answer)
+
+# answer = {
+#     'status': 'exported', 
+#     'data': {
+#         0: 'occupied', 
+#         1: 'occupied', 
+#         2: 'occupied', 
+#         3: 'occupied', 
+#         4: 'occupied', 
+#         5: 'occupied', 
+#         6: 'occupied', 
+#         7: 'occupied', 
+#         8: 'occupied', 
+#         9: 'occupied'
+#         }
+#     }
