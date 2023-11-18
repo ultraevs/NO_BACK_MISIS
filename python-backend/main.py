254b8f052a4987cd976b9aea6974d3bfe35f8bce
root <root@2188653-cf44432.twc1.net>
2023-11-19 00:59:57 +0300

    prepare for database

--- python-backend/main.py
+++ python-backend/main.py
@@ -3,16 +3,16 @@ import json
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
-
+from MainRouter import router as main_router
 
 app = FastAPI(title="URBATON")
 origins = ["*"]
 app.mount("/static", StaticFiles(directory="static/"), name="static")
-
+app.include_router(main_router)
 
 @app.exception_handler(404)
 async def custom_404(_,__):
-    return FileResponse('/static/404.html')
+    return FileResponse('/home/NO_BACK_MISIS/python-backend/static/404.html')
 
 
 app.add_middleware(
