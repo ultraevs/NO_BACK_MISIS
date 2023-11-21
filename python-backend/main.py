42f309e975673d73577b79fa414a23cd21c50881
ultraevs_laptop <evstabpeo@gmail.com>
2023-11-21 11:32:52 +0300

    first auth version

--- python-backend/main.py
+++ python-backend/main.py
@@ -5,12 +5,13 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 from MainRouter import router as main_router
 from HistoryRouter import router as history_router
-
+from auth.router import router as auth_router
 app = FastAPI(title="URBATON")
 origins = ["*"]
 app.mount("/static", StaticFiles(directory="static/"), name="static")
 app.include_router(main_router)
 app.include_router(history_router)
+app.include_router(auth_router)
 
 
 @app.exception_handler(404)
