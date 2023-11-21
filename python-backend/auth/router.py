8cda683d71128894118bdc19a8cf3256be0cc8da
ultraevs <evstabpro@gmail.com>
2023-11-21 18:21:48 +0300

    fix auth

--- python-backend/auth/router.py
+++ python-backend/auth/router.py
@@ -1,9 +1,10 @@
 import sys
 sys.path.append("..")
 from fastapi import Depends, APIRouter, HTTPException
+from fastapi.responses import RedirectResponse
 from fastapi.security import OAuth2PasswordBearer
 from passlib.context import CryptContext
-from auth.manager import *
+from manager import *
 from sqlalchemy.orm import Session
 from database import get_db, engine
 from models import User
@@ -17,20 +18,27 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 @router.post("/register")
 def register_user(username: str, password: str, session: Session = Depends(get_db)):
     hashed_password = pwd_context.hash(password)
-    query = insert(User).values(username=username, hashed_password=hashed_password)
+    # Сохраните пользователя в базе данных
+    query = insert(User).values(username=username,password=password, hashed_password=hashed_password)
     session.execute(query)
     session.commit()
-    return {"username": username, "hashed_password": hashed_password}
+    jwt_token = create_jwt_token({"sub": username})
+    return RedirectResponse(url=f'/me?current_user={jwt_token}')
 
 
 @router.post("/token")
 def authenticate_user(username: str, password: str, session: Session = Depends(get_db)):
     query = select(User).where(User.username == username)
     user = session.execute(query).scalar()
-    if not user or not pwd_context.verify(password, user.hashed_password):
+    if not user:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+
+    is_password_correct = pwd_context.verify(password, user.hashed_password)
+
+    if not is_password_correct:
         raise HTTPException(status_code=400, detail="Incorrect username or password")
     jwt_token = create_jwt_token({"sub": user.username})
-    return {"access_token": jwt_token, "token_type": "bearer"}
+    return RedirectResponse(url=f'/me?current_user={jwt_token}')
 
 
 def get_current_user(token: str = Depends(oauth2_scheme), session: Session = Depends(get_db)):
