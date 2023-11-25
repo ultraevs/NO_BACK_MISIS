2b95acc242e6c4ae099ea471b40cd004388917da
ultraevs <evstabpro@gmail.com>
2023-11-25 11:09:53 +0300

    add res&vanya

--- python-backend/main.py
+++ python-backend/main.py
@@ -5,19 +5,33 @@ from fastapi.responses import FileResponse
 from HistoryOperations.router import router as history_router
 from auth.router import router as auth_router
 from Detection.router import router as detection_router
+from PDDTests.router import router as pdd_router
 import logging
 
 app = FastAPI(title="URBATON")
 origins = ["*"]
 app.mount("/static", StaticFiles(directory="static/"), name="static")
+app.mount("/static/assets", StaticFiles(directory="static/assets"), name="static/assets")
+app.mount("/runs/segment/predict2", StaticFiles(directory="/runs/segment/predict2"), name="/runs/segment/predict2")
 app.include_router(history_router)
 app.include_router(auth_router)
 app.include_router(detection_router)
+app.include_router(pdd_router)
 logging.basicConfig(filename='work.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 @app.get('/')
 async def home():
     return FileResponse('/home/NO_BACK_MISIS/python-backend/static/main.html')
+
+
+@app.get('/vanya')
+async def vanya():
+    return FileResponse("/home/NO_BACK_MISIS/python-backend/static/assets/photo_2023-11-25_11-05-58.jpg")
+
+
+@app.get('/res')
+async def res():
+    return FileResponse("/home/NO_BACK_MISIS/python-backend/runs/segment/predict/img1.jpg")
 
 
 @app.exception_handler(404)
